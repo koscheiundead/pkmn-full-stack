43e8f63b810a08392e24d5a9c442930ceb0202b1
koscheiundead <102054008+koscheiundead@users.noreply.github.com>
2026-03-27 13:57:56 -0700

    add dotenv, open connection pool

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import bodyParser from "body-parser";
+import { Pool } from "pg";
+import 'dotenv/config';
 import type { Request, Response } from "@types/express";
 
 const app = express();
@@ -11,6 +13,14 @@ app.use(cors({ origin: "*" }));
 app.use(bodyParser.json());
 
 const port = 3000;
+
+const pool = new Pool({
+  host: 'localhost',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT)
+});
 
 app.get("/pokemon", (req, res) => {
   res.json({ status: "success!", pokemon: [] });
