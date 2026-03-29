ae4d388e5e83b229bc2b323cd9436b59adb64c98
koscheiundead <102054008+koscheiundead@users.noreply.github.com>
2026-03-28 22:09:27 -0700

    reconfigured pokemon schema per database, updated interface accordingly

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import bodyParser from "body-parser";
-import { Pool } from "pg";
+import { pool } from "../../database/db";
 import 'dotenv/config';
 import type { Request, Response } from "@types/express";
 
@@ -13,14 +13,6 @@ app.use(cors({ origin: "*" }));
 app.use(bodyParser.json());
 
 const port = 3000;
-
-const pool = new Pool({
-  host: 'localhost',
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT)
-});
 
 app.get("/pokemon/:id", async (req: Request, res: Response) => {
   try {
