8e5f43dc7c9bccfbee73e6cb9d969f380b9cbcd4
koscheiundead <102054008+koscheiundead@users.noreply.github.com>
2026-03-29 15:40:22 -0700

    handling seed better, seed now runs successfully

---
+++ server/database/db.ts
@@ -0,0 +1,10 @@
+import { Pool } from "pg";
+import 'dotenv/config';
+
+export const pool = new Pool({
+  host: "localhost",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT),
+});
